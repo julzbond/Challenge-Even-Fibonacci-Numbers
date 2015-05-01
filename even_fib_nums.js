68d97c7bf1969af48ca16914e5368d68061b6297
Ray Farias <ray@rayfarias.com>
2015-04-30 21:52:55 -1000

    update copy

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -1,6 +1,16 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
   var sum = 0;
-  // do your work here
-  
+  var x;
+  for(var i = 0, j = 1, k = 0; k < n; i = j, j = x, k++){
+    if (i >= n) {
+      break;
+    }
+    console.log('i', i);
+    x = i + j;
+    if (x % 2 === 0) {
+      sum += x;
+    }
+    console.log('sum', sum);
+  }
   return sum;
 };
